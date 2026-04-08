8a5bb748c966dab4c8c29caca8c6d645ecc2a6a0
Gabriel <gb.taffarel@gmail.com>
2026-04-08 14:06:15 -0300

    adiciona qutebrowser

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,27 @@
+config.load_autoconfig(False)
+
+# # Search engines
+# c.url.searchengines = {
+#     'DEFAULT':  'https://google.com/search?q={}',
+#     'g':        'https://gemini.google.com/app', # Atalho rápido para o Gemini
+#     'yt':       'https://www.youtube.com/results?search_query={}',
+#     'aw':       'https://wiki.archlinux.org/index.php?search={}',
+#     'cpp':      'https://en.cppreference.com/mwiki/index.php?search={}',
+#     'gh':       'https://github.com/search?q={}',
+# }
+
+# c.url.auto_search = 'naive'
+
+# Binds
+config.bind(",yt", "open -t https://youtube.com")
+config.bind(",ge", "open -t https://gemini.google.com")
+config.bind(",md", "open -t https://moodle.ufrgs.br")
+
+# set dark mode
+# config.set("colors.webpage.darkmode.enabled", True)
+
+# Set nvim as editor
+config.set(
+    "editor.command",
+    ["kitty", "-e", "nvim", "{file}", "-c", "normal {line}G{column0}l"],
+)
